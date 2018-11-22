53e763a282558a5c90c3153af73ed3e6a11de914
sido <sido@haakma.org>
2018-11-22 15:31:29 +0100

    chore(HL7Service): remove unused imports

--- src/main/java/com/synonym/searcher/ontology/backend/HL7ServiceImpl.java
+++ src/main/java/com/synonym/searcher/ontology/backend/HL7ServiceImpl.java
@@ -1,7 +1,6 @@
 package com.synonym.searcher.ontology.backend;
 
 import com.synonym.searcher.ontology.Ontology;
-import com.synonym.searcher.ontology.OntologyTerm;
 import com.synonym.searcher.ontology.aggregator.OntologyService;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +9,9 @@ public class HL7ServiceImpl implements OntologyService {
 
   @Override
   public List<Ontology> fetchOntologies(String searchQuery) {
+    // TODO: consume HL7
+
+    // TODO: Convert HL7 results to ontologies
     List<Ontology> ontologies = new ArrayList<>();
     return ontologies;
   }
